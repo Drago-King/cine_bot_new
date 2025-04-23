47770eb5e89ad8a5cb92b845a9c51a6ffa6b0f7a
Drago-King <106234416+Drago-King@users.noreply.github.com>
2025-04-23 19:07:28 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-    ApplicationBuilder,
     CommandHandler,
     MessageHandler,
     CallbackQueryHandler,
